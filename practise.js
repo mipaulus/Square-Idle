2691c35dc17fa5839e77f19af140c08f0c8f229e
mipaulus <michael.f.paulus@t-online.de>
2024-03-31 03:58:05 +0200

    whatever i did

--- practise.js
+++ practise.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+var counter=document.getElementById("counter");
+
+
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+let counterNumber = 0;
+while (counterNumber < 10000) {
+    await Sleep(3000);
+    counterNumber++;
+    counter.innerHTML = counterNumber.toString();
+}
